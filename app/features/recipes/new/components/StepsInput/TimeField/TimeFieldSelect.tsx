210e8cc77ae78c8b4c456cbe7a4493aebbbd1103
shota <30305829kasukabe@gmail.com>
2025-02-20 21:43:12 +0900

    コードの可読性を向上させるために、冗長なコードを簡素化

--- app/features/recipes/new/components/StepsInput/TimeField/TimeFieldSelect.tsx
+++ app/features/recipes/new/components/StepsInput/TimeField/TimeFieldSelect.tsx
@@ -10,28 +10,22 @@ export const TimeFieldSelect = ({
   selectedValue,
   onValueChange,
   options,
-}: TimeFieldSelectProps) => {
-  const handleValueChange = (value: string) => {
-    onValueChange(value);
-  };
-
-  return (
-    <div className="flex flex-col">
-      <label htmlFor={`${label}-select`} className="mb-1 text-sm font-medium">
-        {label}
-      </label>
-      <select
-        id={`${label}-select`}
-        value={String(selectedValue).padStart(2, '0')}
-        onChange={(e) => handleValueChange(e.target.value)}
-        className="block rounded border border-gray-300 p-2 pr-8 text-black shadow-sm transition-colors duration-200 hover:border-gray-400 focus:outline-none sm:text-sm"
-      >
-        {options.map((option, index) => (
-          <option key={`option-${index}`} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
-    </div>
-  );
-};
+}: TimeFieldSelectProps) => (
+  <div className="flex flex-col">
+    <label htmlFor={`${label}-select`} className="mb-1 text-sm font-medium">
+      {label}
+    </label>
+    <select
+      id={`${label}-select`}
+      value={String(selectedValue).padStart(2, '0')}
+      onChange={(e) => onValueChange(e.target.value)}
+      className="block rounded border border-gray-300 p-2 pr-8 text-black shadow-sm transition-colors duration-200 hover:border-gray-400 focus:outline-none sm:text-sm"
+    >
+      {options.map((option, index) => (
+        <option key={`option-${index}`} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  </div>
+);
